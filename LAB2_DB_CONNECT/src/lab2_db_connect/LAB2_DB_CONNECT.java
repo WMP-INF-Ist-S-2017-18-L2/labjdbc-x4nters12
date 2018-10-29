6615b3da5c0659a5bc342b54fc0234370e667c04
Darek <Darek@MSI-LAPTOP>
2018-10-30 00:39:26 +0100

    start

--- LAB2_DB_CONNECT/src/lab2_db_connect/LAB2_DB_CONNECT.java
+++ LAB2_DB_CONNECT/src/lab2_db_connect/LAB2_DB_CONNECT.java
@@ -5,6 +5,20 @@ package lab2_db_connect;
  */
 public class LAB2_DB_CONNECT {
     public static void main(String[] args) {
+        postgresqlConnect();
+        //mysqlConnect();
+        //sqliteConnect();
+    }
+    
+    private static void postgresqlConnect(){
+        
+    }
+    
+    private static void mysqlConnect(){
+        
+    }
+    
+    private static void sqliteConnect(){
         
     }
 }
