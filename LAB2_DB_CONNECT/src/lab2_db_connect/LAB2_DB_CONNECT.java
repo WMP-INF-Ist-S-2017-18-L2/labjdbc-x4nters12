20033a5b510ca843ff9250510f3606206e10d474
Darek <Darek@MSI-LAPTOP>
2018-10-29 22:59:05 +0100

    start

---
+++ LAB2_DB_CONNECT/src/lab2_db_connect/LAB2_DB_CONNECT.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab2_db_connect;
+
+/**
+ *
+ * @author Darek
+ */
+public class LAB2_DB_CONNECT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
